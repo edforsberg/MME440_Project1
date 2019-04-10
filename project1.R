eabfea736d06f1d7295b199bc42721111370434e
verbro <31824207+verbro@users.noreply.github.com>
2019-04-10 16:19:16 +0200

    Update project1.R

--- project1.R
+++ project1.R
@@ -3,12 +3,30 @@ options(warn=1)
 library(tidyverse)
 library(latex2exp) # Latex in ggplot2 labels
 cbPalette <- c(
-  "#999999", "#E69F00", "#56B4E9", "#009E73",
-  "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+  "#da9033",
+  "#6d6bd1",
+  "#85b937",
+  "#b959c8",
+  "#52c257",
+  "#d13f80",
+  "#63c086",
+  "#d04d36",
+  "#41c0c7",
+  "#bb5663",
+  "#4a8a36",
+  "#925391",
+  "#bcae37",
+  "#6b8ecd",
+  "#717229",
+  "#dc88c1",
+  "#388661",
+  "#e09571",
+  "#a9af63",
+  "#9b5f2c")
 #datas<-read.csv("http://mlr.cs.umass.edu/ml/machine-learning-databases/adult/adult.data", header=TRUE, sep=",")
 set.seed(102)
 n = 20
-nrClass = 6
+nrClass = 8
 myData <- data.frame(x1=double(),x2=double(),Y=integer())
 for(i in 1:floor(nrClass/2)){
 k = i*2
@@ -27,15 +45,15 @@ colnames(myData)<-c("x1","x2","Y")
 
 ggplot(myData, aes(x = x1, y = x2, colour = Y)) +
   geom_point(size = 1.5) +
-  scale_colour_manual("Class",values = cbPalette[-1]) +
+  scale_colour_manual("Class", values = cbPalette) +
   scale_x_continuous(TeX("x_1"), lim = c(10, 20+5*nrClass), expand = c(0, 0)) +
   scale_y_continuous(TeX("x_2"), lim = c(10, 40), expand = c(0, 0)) +
   theme_minimal() +
   theme(legend.position = "right") +
   coord_fixed()
 
-h <- 0.3 
-x1_arr <- seq(10, 50, by = h)
+h <- 0.2 
+x1_arr <- seq(10, 20+5*nrClass, by = h)
 x2_arr <- seq(10, 40, by = h)
 myTest <- expand.grid(x1_arr, x2_arr)
 
@@ -62,15 +80,11 @@ ggplot() +
     aes(x = x1, y = x2, colour = Y, fill = Y),
     data = myData, size = 1.5) +
   facet_wrap(~ 5, ncol = 1) +
-  scale_colour_manual("Class", values = cbPalette[-1]) +
-  scale_fill_manual("Class", values = cbPalette[-1], guide = FALSE) +
+  scale_colour_manual("Class", values = cbPalette) +
+  scale_fill_manual("Class", values = cbPalette, guide = FALSE) +
   scale_x_continuous(TeX("x_1"), lim = c(10, 20+5*nrClass), expand = c(0, 0)) +
   scale_y_continuous(TeX("x_2"), lim = c(10, 40), expand = c(0, 0)) +
-  theme(
-    legend.position = "bottom",
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank()) +
+  theme(legend.position = "right") +
   coord_fixed()
 
 fit_lda <- MASS::lda(Y ~ x1 + x2, myTest)
@@ -92,14 +106,10 @@ ggplot() +
   geom_point(
     aes(x = x1, y = x2, colour = Y, fill = Y),
     data = myData, size = 1.5) +
-  scale_colour_manual("Class", values = cbPalette[-1]) +
-  scale_fill_manual("Class", values = cbPalette[-1], guide = FALSE) +
+  scale_colour_manual("Class", values = cbPalette) +
+  scale_fill_manual("Class", values = cbPalette, guide = FALSE) +
   scale_x_continuous(TeX("x_1"), lim = c(10, 20+5*nrClass), expand = c(0, 0)) +
   scale_y_continuous(TeX("x_2"), lim = c(10, 40), expand = c(0, 0)) +
-  theme(
-    legend.position = "bottom",
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank()) +
+  theme(legend.position = "right") +
   coord_fixed()
   ggtitle("LDA")
