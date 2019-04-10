6b47acba4363264a6c5fa0af9b4f758590d2a8e0
Erik Forsberg <erikforsberg95@hotmail.com>
2019-04-10 16:43:41 +0200

    added create classes function

--- project1.R
+++ project1.R
@@ -24,24 +24,7 @@ cbPalette <- c(
   "#a9af63",
   "#9b5f2c")
 #datas<-read.csv("http://mlr.cs.umass.edu/ml/machine-learning-databases/adult/adult.data", header=TRUE, sep=",")
-set.seed(102)
-n = 20
-nrClass = 8
-myData <- data.frame(x1=double(),x2=double(),Y=integer())
-for(i in 1:floor(nrClass/2)){
-k = i*2
-x11 = rnorm(n, mean = 20+10*(i-1), sd = 3)
-x21 = rnorm(n, mean = 20, sd = 3)
-x12 = rnorm(n, mean = 20+10*(i-1), sd = 3)
-x22 = rnorm(n, mean = 30, sd = 3)
-Y1 = rep((k-1),n)
-Y2 = rep(k,n)
-myData <- data.frame(x1=c(myData$x1,x11,x12),
-                     x2=c(myData$x2,x21,x22),
-                     Y=c(myData$Y,Y1,Y2))
-}
-myData$Y<- as.factor(myData$Y)
-colnames(myData)<-c("x1","x2","Y")
+myData = CreateClasses(8, 20, 3, 102)
 
 ggplot(myData, aes(x = x1, y = x2, colour = Y)) +
   geom_point(size = 1.5) +
@@ -113,3 +96,32 @@ ggplot() +
   theme(legend.position = "right") +
   coord_fixed()
   ggtitle("LDA")
+  
+  #------------------------------# create test data
+  
+  CreateClasses <- function(nrOfClasses, nrOfDataPts, var, seed) {
+    {
+      
+      set.seed(seed)
+      n = nrOfDataPts
+      nrClass = nrOfClasses
+      myData <- data.frame(x1=double(),x2=double(),Y=integer())
+      for(i in 1:floor(nrClass/2)){
+        k = i*2
+        x11 = rnorm(n, mean = 20+10*(i-1), sd = 3)
+        x21 = rnorm(n, mean = 20, sd = 3)
+        x12 = rnorm(n, mean = 20+10*(i-1), sd = 3)
+        x22 = rnorm(n, mean = 30, sd = 3)
+        Y1 = rep((k-1),n)
+        Y2 = rep(k,n)
+        myData <- data.frame(x1=c(myData$x1,x11,x12),
+                             x2=c(myData$x2,x21,x22),
+                             Y=c(myData$Y,Y1,Y2))
+      }
+      myData$Y<- as.factor(myData$Y)
+      colnames(myData)<-c("x1","x2","Y")
+      
+      
+      return(mydata)
+      
+    }
