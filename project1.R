c21babcfad2227efd36b3fb301c3bfcc03ac306b
verbro <31824207+verbro@users.noreply.github.com>
2019-04-11 17:37:46 +0200

    Update project1.R

--- project1.R
+++ project1.R
@@ -43,35 +43,44 @@ CreateCircleData <- function(nrClass, nrOfDataPts, std, seed){
   return(myData)
 }
 
-CreateData <- function(nrOfClasses, nrOfDataPts, std, seed){
-    set.seed(seed)
-    n = nrOfDataPts
-    nrClass = nrOfClasses
-    myData <- data.frame(x1=double(),x2=double(),Y=integer())
-    for(i in 1:floor(nrClass/2)){
-      k = i*2
-      x11 = rnorm(n, mean = 20+10*(i-1), sd = std)
-      x21 = rnorm(n, mean = 20, sd = std)
-      x12 = rnorm(n, mean = 20+10*(i-1), sd = std)
-      x22 = rnorm(n, mean = 30, sd = std)
-      Y1 = rep((k-1),n)
-      Y2 = rep(k,n)
-      myData <- data.frame(x1=c(myData$x1,x11,x12),
-                           x2=c(myData$x2,x21,x22),
-                           Y=c(myData$Y,Y1,Y2))
-    }
-    myData$Y<- as.factor(myData$Y)
-    colnames(myData)<-c("x1","x2","Y")
-    myData <- myData[sample(nrow(myData)),]
-    return(myData)
+Create9CData <- function(nrOfDataPts, std, seed){
+  set.seed(seed)
+  n = nrOfDataPts/9
+  atclass=0
+  myData <- data.frame(x1=double(),x2=double(),Y=integer())
+  for(i in 1:3){
+    for(j in 1:3){
+    atclass= atclass +1
+    x1 = rnorm(n, mean = (10*i - 20), sd = std)
+    x2 = rnorm(n, mean = (10*j - 20), sd = std)
+    Y = rep(atclass,n)
+    myData <- data.frame(x1=c(myData$x1,x1),
+                         x2=c(myData$x2,x2),
+                         Y=c(myData$Y,Y))
+  }
+  }
+  myData$Y<- as.factor(myData$Y)
+  colnames(myData)<-c("x1","x2","Y")
+  myData <- myData[sample(nrow(myData)),]
+  return(myData)
   }
 #----------Plot points#
 n=480
 nrClass=16
 std = 4
-myData = CreateCircleData(nrClass, n, std, 102)
 
+myData = CreateCircleData(nrClass, n, std, 102)
 ggplot(myData, aes(x = x1, y = x2, colour = Y)) +
+  geom_point(size = 1.5) +
+  scale_colour_manual("Class", values = cbPalette) +
+  scale_x_continuous(TeX("x_1"), lim = c(-25, 25), expand = c(0, 0)) +
+  scale_y_continuous(TeX("x_2"), lim = c(-25, 25), expand = c(0, 0)) +
+  theme_minimal() +
+  theme(legend.position = "right") +
+  coord_fixed()
+
+myQuadData = Create9CData(n, std, 59)
+ggplot(myQuadData, aes(x = x1, y = x2, colour = Y)) +
   geom_point(size = 1.5) +
   scale_colour_manual("Class", values = cbPalette) +
   scale_x_continuous(TeX("x_1"), lim = c(-25, 25), expand = c(0, 0)) +
@@ -139,7 +148,63 @@ ggplot() +
   theme(legend.position = "right") +
   coord_fixed()
 
-#ggtitle("LDA")  
+#----------New Geometri#
+myGrid <- expand.grid(x1_arr, x2_arr)
+pred <- lapply(5, function(k) {
+  class::knn(
+    as.matrix(myQuadData[,1:2]),   # training data (variables)
+    myGrid,                     # test data (variables)
+    as.integer(myQuadData$Y),  # training data (classes)
+    k = k)                         # k
+})
+
+myGrid <- tibble(
+  x1 = rep.int(myGrid[,1], 1),
+  x2 = rep.int(myGrid[,2], 1),
+  Y = as.factor(do.call(c, pred)))
+
+ggplot() +
+  geom_tile(
+    aes(x = x1, y = x2, fill = Y),
+    data = myGrid, alpha = 0.5,
+    width = h, height = h,
+    colour = "transparent") +
+  geom_point(
+    aes(x = x1, y = x2, colour = Y, fill = Y),
+    data = myQuadData, size = 1.5) +
+  scale_colour_manual("Class", values = cbPalette) +
+  scale_fill_manual("Class", values = cbPalette, guide = FALSE) +
+  scale_x_continuous(TeX("x_1"), lim = c(-25, 25), expand = c(0, 0)) +
+  scale_y_continuous(TeX("x_2"), lim = c(-25, 25), expand = c(0, 0)) +
+  theme(legend.position = "right") +
+  coord_fixed()
+
+fit_lda <- MASS::lda(Y ~ x1 + x2, myQuadData)
+myGrid <- expand.grid(x1_arr, x2_arr)
+colnames(myGrid) <- c("x1", "x2")
+myGrid <- tibble(
+  x1 = rep.int(myGrid[,1], 1),
+  x2 = rep.int(myGrid[,2], 1),
+  Y = predict(fit_lda, myGrid)$class)
+
+ggplot() +
+  geom_tile(
+    aes(x = x1, y = x2, colour = Y, fill = Y),
+    colour = "transparent",
+    data = myGrid,
+    width = h, height = h,
+    alpha = 0.5) +
+  geom_point(
+    aes(x = x1, y = x2, colour = Y, fill = Y),
+    data = myQuadData, size = 1.5) +
+  scale_colour_manual("Class", values = cbPalette) +
+  scale_fill_manual("Class", values = cbPalette, guide = FALSE) +
+  scale_x_continuous(TeX("x_1"), lim = c(-25, 25), expand = c(0, 0)) +
+  scale_y_continuous(TeX("x_2"), lim = c(-25, 25), expand = c(0, 0)) +
+  theme(legend.position = "right") +
+  coord_fixed()
+
+
 #----------Errors#
   
 errorFuncKNN <- function(data,n,k){
@@ -190,29 +255,29 @@ print(errorFuncLDA(myTest1,5))
 
 #-------------------------------#
 
-classVector = 1:6*3
+classVector = 2:20
 nrDataPts = 480
 std = 4
 seed = 1
-nrFolds = 10
-k1 = 4
+nrFolds = 5
+k1 = 5
 k2 = 20
 
 
 calcError <- function(classVector,nrDataPts,std,seed, nrFolds, k1, k2){
-  knnErrors1 <- vector(mode="numeric", length=6)
-  knnErrors2 <- vector(mode="numeric", length=6)
+  knnErrors1 <- rep(0,6)
+  knnErrors2 <- rep(0,6)
+  ldaErrors <- rep(0.6)
   counter = 0
   for (i in classVector){
     counter = counter+1
-    data = CreateData(i,nrDataPts,std,seed)
-    knnErrors1[index] = errorfuncKNN(data, nrFolds,k1)
-    knnErrors2[index] = errorfuncKNN(data, nrFolds,k2)
-    ldaErrors[index] = errorFuncLDA(data, nrFolds)
+    data = Create9CData(nrDataPts,std,seed)
+    knnErrors1[counter] = errorFuncKNN(data, nrFolds,k1)
+    knnErrors2[counter] = errorFuncKNN(data, nrFolds,k2)
+    ldaErrors[counter] = errorFuncLDA(data, nrFolds)
   }
   errorData = data.frame(knnErrors1,knnErrors2,ldaErrors)
-  return(errorData)
-  
+  errorData
 }
 print(calcError(classVector, nrDataPts, std, seed, nrFolds, k1,k2))
 
