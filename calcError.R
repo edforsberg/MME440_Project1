625bab2ecf5c63c1253256e20fba1c8df1c79b60
Erik Forsberg <erikforsberg95@hotmail.com>
2019-04-11 15:10:33 +0200

    k1

--- calcError.R
+++ calcError.R
@@ -1,16 +1,26 @@
-classVec = c(2,4,8,12,16,20)
+classVector = 1:6*3
 nrDataPts = 480
+std = 4
+seed = 1
+nrFolds = 10
+k1 = 4
+k2 = 20
 
 
-
-calcError <- function(classVector,nrDataPts,std,seed, nrFolds){
-  knnError = 
-  for (i in classVector)
-    
+calcError <- function(classVector,nrDataPts,std,seed, nrFolds, k1, k2){
+  knnErrors1 <- vector(mode="numeric", length=6)
+  knnErrors2 <- vector(mode="numeric", length=6)
+  index = 0
+  for (i in classVector){
+    index = index+1
     data = CreateData(i,nrDataPts,std,seed)
-  errorKNN = errorfuncKNN(data, nrFolds)
-  
-  
-  
+    knnErrors1[[index]] = errorfuncKNN(data, nrFolds,k1)
+    knnErrors2[[index]] = errorfuncKNN(data, nrFolds,k2)
+    ldaErrors[[index]] = errorFuncLDA(data, nrDataPts)
+  }
+  errorData = data.frame(knnErrors1,knnErrors2,ldaErrors)
+  return(errorData)
   
 }
+print(calcError(classVector, nrDataPts, std, seed, nrFolds, k1,k2))
+
--- project1.R
+++ project1.R
@@ -190,3 +190,30 @@ print(errorFuncKNN(myTest1,5,5))
 print(errorFuncLDA(myTest1,5))
 
 #-------------------------------#
+
+classVector = 1:6*3
+nrDataPts = 480
+std = 4
+seed = 1
+nrFolds = 10
+k1 = 4
+k2 = 20
+
+
+calcError <- function(classVector,nrDataPts,std,seed, nrFolds, k1, k2){
+  knnErrors1 <- vector(mode="numeric", length=6)
+  knnErrors2 <- vector(mode="numeric", length=6)
+  counter = 0
+  for (i in classVector){
+    counter = counter+1
+    data = CreateData(i,nrDataPts,std,seed)
+    knnErrors1[index] = errorfuncKNN(data, nrFolds,k1)
+    knnErrors2[index] = errorfuncKNN(data, nrFolds,k2)
+    ldaErrors[index] = errorFuncLDA(data, nrFolds)
+  }
+  errorData = data.frame(knnErrors1,knnErrors2,ldaErrors)
+  return(errorData)
+  
+}
+print(calcError(classVector, nrDataPts, std, seed, nrFolds, k1,k2))
+
