c281f3f77e495aa5cadc6e288ead465e14e4704e
verbro <31824207+verbro@users.noreply.github.com>
2019-04-12 09:32:08 +0200

    Update project1.R

--- project1.R
+++ project1.R
@@ -1,7 +1,9 @@
 cat("\014")
 options(warn=1)
+#ggsave("filename.eps", device=cairo_ps) save image
 library(tidyverse)
-library(latex2exp) # Latex in ggplot2 labels
+library(latex2exp)
+library(FNN)
 cbPalette <- c(
   "#da9033",
   "#6d6bd1",
@@ -22,10 +24,10 @@ cbPalette <- c(
   "#388661",
   "#e09571",
   "#a9af63",
-  "#9b5f2c")
+  "#9b5f2c") #Randomly generated color values 
 #datas<-read.csv("http://mlr.cs.umass.edu/ml/machine-learning-databases/adult/adult.data", header=TRUE, sep=",")
 #------------------------------# create test data
-CreateCircleData <- function(nrClass, nrOfDataPts, std, seed){
+CreateCircleData <- function(nrClass, nrOfDataPts, std, seed){ # Create data points were classes form a circle
   set.seed(seed)
   n = nrOfDataPts/nrClass
   myData <- data.frame(x1=double(),x2=double(),Y=integer())
@@ -39,20 +41,20 @@ CreateCircleData <- function(nrClass, nrOfDataPts, std, seed){
   }
   myData$Y<- as.factor(myData$Y)
   colnames(myData)<-c("x1","x2","Y")
-  myData <- myData[sample(nrow(myData)),]
+  myData <- myData[sample(nrow(myData)),] #randomly samlpe order to data to use in evaluation
   return(myData)
 }
 
-Create9CData <- function(nrOfDataPts, std, seed){
+CreateQuadData <- function(sides, nrOfDataPts, std, seed){ # Create dataset were classes form a square 
   set.seed(seed)
-  n = nrOfDataPts/9
+  n = nrOfDataPts/(sides*sides)
   atclass=0
   myData <- data.frame(x1=double(),x2=double(),Y=integer())
-  for(i in 1:3){
-    for(j in 1:3){
+  for(i in 1:sides){
+    for(j in 1:sides){
     atclass= atclass +1
-    x1 = rnorm(n, mean = (10*i - 20), sd = std)
-    x2 = rnorm(n, mean = (10*j - 20), sd = std)
+    x1 = rnorm(n, mean = (10*(i - (1/2 + sides/2))), sd = std)
+    x2 = rnorm(n, mean = (10*(j - (1/2 + sides/2))), sd = std)
     Y = rep(atclass,n)
     myData <- data.frame(x1=c(myData$x1,x1),
                          x2=c(myData$x2,x2),
@@ -61,16 +63,16 @@ Create9CData <- function(nrOfDataPts, std, seed){
   }
   myData$Y<- as.factor(myData$Y)
   colnames(myData)<-c("x1","x2","Y")
-  myData <- myData[sample(nrow(myData)),]
+  myData <- myData[sample(nrow(myData)),]  #randomly samlpe order to data to use in evaluation
   return(myData)
-  }
+}
 #----------Plot points#
-n=480
-nrClass=16
+n=480 #Create values for dataset
+nrClass=2
 std = 4
 
-myData = CreateCircleData(nrClass, n, std, 102)
-ggplot(myData, aes(x = x1, y = x2, colour = Y)) +
+myData = CreateCircleData(nrClass, n, std, 100) #Circle data used for plots
+ggplot(myData, aes(x = x1, y = x2, colour = Y)) + #plot of circle dataset
   geom_point(size = 1.5) +
   scale_colour_manual("Class", values = cbPalette) +
   scale_x_continuous(TeX("x_1"), lim = c(-25, 25), expand = c(0, 0)) +
@@ -79,8 +81,8 @@ ggplot(myData, aes(x = x1, y = x2, colour = Y)) +
   theme(legend.position = "right") +
   coord_fixed()
 
-myQuadData = Create9CData(n, std, 59)
-ggplot(myQuadData, aes(x = x1, y = x2, colour = Y)) +
+myQuadData = CreateQuadData(3,n, std, 59) #Square shaped data set used for plots
+ggplot(myQuadData, aes(x = x1, y = x2, colour = Y)) + #plot of square dataset
   geom_point(size = 1.5) +
   scale_colour_manual("Class", values = cbPalette) +
   scale_x_continuous(TeX("x_1"), lim = c(-25, 25), expand = c(0, 0)) +
@@ -89,17 +91,17 @@ ggplot(myQuadData, aes(x = x1, y = x2, colour = Y)) +
   theme(legend.position = "right") +
   coord_fixed()
 
-h <- 0.2 
+h <- 0.2
 x1_arr <- seq(-25, 25, by = h)
 x2_arr <- seq(-25, 25, by = h)
 myGrid <- expand.grid(x1_arr, x2_arr)
 
-pred <- lapply(5, function(k) {
-  class::knn(
-    as.matrix(myData[,1:2]),   # training data (variables)
-    myGrid,                     # test data (variables)
-    as.integer(myData$Y),  # training data (classes)
-    k = k)                         # k
+pred <- lapply(5, function(k) {#knn r-function
+  FNN::knn(
+    as.matrix(myData[,1:2]),   
+    myGrid,                     
+    as.integer(myData$Y),  
+    k = k)                         
 })
 
 myGrid <- tibble(
@@ -107,7 +109,7 @@ myGrid <- tibble(
   x2 = rep.int(myGrid[,2], 1),
   Y = as.factor(do.call(c, pred)))
 
-ggplot() +
+ggplot() + #plot of circle dataset with knn decision boundarys
   geom_tile(
     aes(x = x1, y = x2, fill = Y),
     data = myGrid, alpha = 0.5,
@@ -123,7 +125,7 @@ ggplot() +
   theme(legend.position = "right") +
   coord_fixed()
 
-fit_lda <- MASS::lda(Y ~ x1 + x2, myData)
+fit_lda <- MASS::lda(Y ~ x1 + x2, myData) #LDA fit function
 myGrid <- expand.grid(x1_arr, x2_arr)
 colnames(myGrid) <- c("x1", "x2")
 myGrid <- tibble(
@@ -131,7 +133,7 @@ myGrid <- tibble(
   x2 = rep.int(myGrid[,2], 1),
   Y = predict(fit_lda, myGrid)$class)
 
-ggplot() +
+ggplot() + #plot of circle dataset with LDA decision boundarys
   geom_tile(
     aes(x = x1, y = x2, colour = Y, fill = Y),
     colour = "transparent",
@@ -149,13 +151,14 @@ ggplot() +
   coord_fixed()
 
 #----------New Geometri#
+#---quad#
 myGrid <- expand.grid(x1_arr, x2_arr)
 pred <- lapply(5, function(k) {
-  class::knn(
-    as.matrix(myQuadData[,1:2]),   # training data (variables)
-    myGrid,                     # test data (variables)
-    as.integer(myQuadData$Y),  # training data (classes)
-    k = k)                         # k
+  FNN::knn(
+    as.matrix(myQuadData[,1:2]),   
+    myGrid,                     
+    as.integer(myQuadData$Y),  
+    k = k)                         
 })
 
 myGrid <- tibble(
@@ -163,7 +166,7 @@ myGrid <- tibble(
   x2 = rep.int(myGrid[,2], 1),
   Y = as.factor(do.call(c, pred)))
 
-ggplot() +
+ggplot() + #plot of square dataset with knn decision boundarys
   geom_tile(
     aes(x = x1, y = x2, fill = Y),
     data = myGrid, alpha = 0.5,
@@ -187,7 +190,7 @@ myGrid <- tibble(
   x2 = rep.int(myGrid[,2], 1),
   Y = predict(fit_lda, myGrid)$class)
 
-ggplot() +
+ggplot() + #plot of square dataset with LDA decision boundarys
   geom_tile(
     aes(x = x1, y = x2, colour = Y, fill = Y),
     colour = "transparent",
@@ -204,10 +207,9 @@ ggplot() +
   theme(legend.position = "right") +
   coord_fixed()
 
-
 #----------Errors#
   
-errorFuncKNN <- function(data,n,k){
+errorFuncKNN <- function(data,n,k){ #function to retorn arror rate for knn
   nrcl = nlevels(data$Y)
   error = 0
   len = length(data[,1])
@@ -219,10 +221,10 @@ errorFuncKNN <- function(data,n,k){
     train$Y<-factor(train$Y, levels = 1:nrcl)
     pred_test <- lapply(k, function(k) {
       class::knn(
-        as.matrix(train[,1:2]),   # training data (variables)
-        as.matrix(test[,1:2]),  # test data (variables)
-        as.integer(train$Y),  # training data (classes)
-        k = k)# k
+        as.matrix(train[,1:2]),   
+        as.matrix(test[,1:2]),  
+        as.integer(train$Y),  
+        k = k)
     })
     pred_Y <- as.factor(do.call(c, pred_test))
     pred_Y<-factor(pred_Y, levels = 1:nrcl)
@@ -231,7 +233,7 @@ errorFuncKNN <- function(data,n,k){
   error/len
 }
 
-errorFuncLDA <- function(data,n){
+errorFuncLDA <- function(data,n){ #function to retorn arror rate for LDA
   nrcl = nlevels(data$Y)
   error = 0 
   len = length(data[,1])
@@ -248,60 +250,10 @@ errorFuncLDA <- function(data,n){
   }
   error/len
 }
-
-myTest1 <- CreateData(8, 20, 3, 103)
-print(errorFuncKNN(myTest1,5,5))
-print(errorFuncLDA(myTest1,5))
 
 #-------------------------------#
 
-
-#----------Errors#
-
-errorFuncKNN <- function(data,n,k){
-  nrcl = nlevels(data$Y)
-  error = 0
-  len = length(data[,1])
-  for(i in 1:n){
-    ii = ((i-1)*len/n+1):(i*len/n)
-    test <- data[ii,]
-    test$Y <- factor(test$Y, levels = 1:nrcl)
-    train <- data[-ii,]
-    train$Y<-factor(train$Y, levels = 1:nrcl)
-    pred_test <- lapply(k, function(k) {
-      class::knn(
-        as.matrix(train[,1:2]),   # training data (variables)
-        as.matrix(test[,1:2]),  # test data (variables)
-        as.integer(train$Y),  # training data (classes)
-        k = k)# k
-    })
-    pred_Y <- as.factor(do.call(c, pred_test))
-    pred_Y<-factor(pred_Y, levels = 1:nrcl)
-    error = error + length(which(test$Y != pred_Y))
-  }
-  error/len
-}
-
-errorFuncLDA <- function(data,n){
-  nrcl = nlevels(data$Y)
-  error = 0 
-  len = length(data[,1])
-  for(i in 1:n){
-    ii = ((i-1)*len/n+1):(i*len/n)
-    test <- data[ii,]
-    test$Y <- factor(test$Y, levels = 1:nrcl)
-    train <- data[-ii,]
-    train$Y<-factor(train$Y, levels = 1:nrcl)
-    fit_lda <- MASS::lda(Y ~ x1 + x2, train)
-    pred_Y = predict(fit_lda, test)$class
-    pred_Y<-factor(pred_Y, levels = 1:nrcl)
-    error = error+length(which(test$Y != pred_Y))
-  }
-  error/len
-}
-
-######n=480#####
-classVector = 2:20
+classVector = 2:20 #Values for error computations
 nrDataPts = 480
 std = 4
 seed = 100
@@ -310,25 +262,25 @@ k1 = 5
 k2 = 20
 
 
-calcError <- function(classVector,nrDataPts,std,seed, nrFolds, k1, k2){
-  knnErrors1 <- rep(0,6)
-  knnErrors2 <- rep(0,6)
-  ldaErrors <- rep(0,6)
+calcError <- function(classVector,nrDataPts,std,seed, nrFolds, k1, k2){ #Calculate error to a data.frame
+  knnErrors1 <- rep(0)
+  knnErrors2 <- rep(0)
+  ldaErrors <- rep(0)
   counter = 0
   for (i in classVector){
     counter = counter+1
     data = CreateCircleData(i,nrDataPts,std,seed)
-    knnErrors1[counter] = round(errorFuncKNN(data, nrFolds,k1)*100, 2)
-    knnErrors2[counter] = round(errorFuncKNN(data, nrFolds,k2)*100, 2)
-    ldaErrors[counter] = round(errorFuncLDA(data, nrFolds)*100, 2)
+    knnErrors1[counter] = errorFuncKNN(data, nrFolds,k1) #errors for knn (=5)
+    knnErrors2[counter] = errorFuncKNN(data, nrFolds,k2) #errors for knn (=20)
+    ldaErrors[counter] = errorFuncLDA(data, nrFolds) #errors LDA
   }
   errorData = data.frame(knnErrors1,knnErrors2,ldaErrors)
   errorData
 }
+print(calcError(classVector, nrDataPts, std, seed, nrFolds, k1,k2))
 
-calcError(classVector, nrDataPts, std,seed, nrFolds, k1, k2)
 
-Create.plot<-function(classVector,k1,k2){
+Create.plot<-function(classVector,k1,k2){#create plot of errors when varying nr classes
   # Generate some data
   x<-classVector; 
   df=calcError(classVector, nrDataPts, std, seed, nrFolds, k1,k2)
@@ -343,11 +295,6 @@ Create.plot<-function(classVector,k1,k2){
   # Add a line
   lines(x, y3, pch=18, col="blue", type="b", lty=2)
   
-  #xtick<-c(2, 4, 8, 12, 16, 20)
-  #axis(side=1, at=xtick, labels = FALSE)
-  #text(x=xtick,  par("usr")[3], 
-  #     labels = xtick, las=1, srt = 0, pos = 1, xpd = TRUE)
-  
   # Add a legend
   legend("topleft", legend=c("kNN(k=5)", "kNN(k=20)", "LDA"),
          col=c("red", "green", "blue"), lty=1:2, cex=0.8)
@@ -355,62 +302,3 @@ Create.plot<-function(classVector,k1,k2){
 }
 
 plot1=Create.plot(2:20, 5, 20)
-
-############n=960#############
-classVector = 2:20
-nrDataPts = 960
-std = 4
-seed = 100
-nrFolds = 10
-k1 = 5
-k2 = 20
-
-
-calcError <- function(classVector,nrDataPts,std,seed, nrFolds, k1, k2){
-  knnErrors1 <- rep(0,6)
-  knnErrors2 <- rep(0,6)
-  ldaErrors <- rep(0,6)
-  counter = 0
-  for (i in classVector){
-    counter = counter+1
-    data = CreateCircleData(i,nrDataPts,std,seed)
-    knnErrors1[counter] = round(errorFuncKNN(data, nrFolds,k1)*100, 2)
-    knnErrors2[counter] = round(errorFuncKNN(data, nrFolds,k2)*100, 2)
-    ldaErrors[counter] = round(errorFuncLDA(data, nrFolds)*100, 2)
-  }
-  errorData = data.frame(knnErrors1,knnErrors2,ldaErrors)
-  errorData
-}
-
-calcError(classVector, nrDataPts, std,seed, nrFolds, k1, k2)
-
-Create.plot<-function(classVector,k1,k2){
-  # Generate some data
-  x<-classVector; 
-  df=calcError(classVector, nrDataPts, std, seed, nrFolds, k1,k2)
-  y1<-df[, 1]; 
-  y2<-df[, 2];
-  y3<-df[, 3];
-  p.plot<-plot(x, y1, type="b", pch=19, col="red", main="Error plot(n=960)", xlab="Number of classes", ylab="Percentage of errors")#, xaxt='n', ticks=TRUE)
-  
-  # Add a line
-  lines(x, y2, pch=18, col="green", type="b", lty=2)
-  
-  # Add a line
-  lines(x, y3, pch=18, col="blue", type="b", lty=2)
-  
-  #xtick<-c(2, 4, 8, 12, 16, 20)
-  #axis(side=1, at=xtick, labels = FALSE)
-  #text(x=xtick,  par("usr")[3], 
-  #     labels = xtick, las=1, srt = 0, pos = 1, xpd = TRUE)
-  
-  # Add a legend
-  legend("topleft", legend=c("kNN(k=5)", "kNN(k=20)", "LDA"),
-         col=c("red", "green", "blue"), lty=1:2, cex=0.8)
-  return(p.plot)  
-}
-
-plot2=Create.plot(2:20, 5, 20)
-
-
-
